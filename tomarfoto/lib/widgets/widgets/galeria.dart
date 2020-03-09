bdc8d287bef252b3f2fa0289089d840dba902caf
Carlos Garzon <54120609+CarlosUngi@users.noreply.github.com>
2020-03-09 14:36:24 -0500

    servicio Galeria

--- tomarfoto/lib/widgets/widgets/galeria.dart
+++ tomarfoto/lib/widgets/widgets/galeria.dart
@@ -1,6 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:tomarfoto/provider/pathProvider.dart';
-import 'dart:convert';
+import 'package:path_provider/path_provider.dart';
 
 class Galeria extends StatefulWidget {
   @override
@@ -11,21 +12,31 @@ class _GaleriaState extends State<Galeria> {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: listaPath(),
-      builder: (context,lista){
-        List<dynamic> listajson = json.decode(lista.data); 
-        List<dynamic> listaPaths = listajson.first['files'];
-        print(listaPaths.first.toString());
-        return imprimirPath(lista.data);
+      future: listaPath2(),
+      builder: (context, lista) {
+        List<dynamic> listaPaths = lista.data;
+        print(listaPaths.first);
+        return ListView.builder(
+            itemCount: listaPaths.length, 
+            itemBuilder: (context, index) {
+              return Image.file(listaPaths.elementAt(index));
+            });
       },
     );
   }
 }
 
+Widget imprimirPath(String pathlist) {
+  return SingleChildScrollView(
+    child: Column(
+      children: <Widget>[Text(pathlist)],
+    ),
+  );
+}
 
-Widget imprimirPath(String pathlist){
-
-  return SingleChildScrollView(child: Column(children: <Widget>[
-    Text(pathlist)
-  ],),);
+Future<List<FileSystemEntity>> listaPath2() async {
+  final Directory extDir = await getApplicationDocumentsDirectory();
+  final String dirPath = '${extDir.path}/Pictures/flutter_test';
+  var ret = await Directory(dirPath).list();
+  return ret.toList();
 }
