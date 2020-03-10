ffa25cb89f96f82dda71f339f6439a57a5eca7f4
dktorres1995 <59892053+dktorres1995@users.noreply.github.com>
2020-03-10 11:46:03 -0500

    Ajustes visuales

--- tomarfoto/lib/screens/historial.dart
+++ tomarfoto/lib/screens/historial.dart
@@ -8,54 +8,33 @@ import 'package:tomarfoto/screens/envioImagen.dart';
 
 import 'TraerInfo.dart';
 
-
 class Historial extends StatelessWidget {
   static const routedName = '/historial';
   @override
-   
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      
-        appBar: AppBar(
-          title: Text('NUMERATE'),
-          centerTitle: true,
-           backgroundColor: Theme.of(context).accentColor,
-        ),
-        body: Center(
-           child: FutureBuilder(
-            future: obtener(),
-            builder: (context, AsyncSnapshot <List<Recursos>> snapshot) {
-              
-              if (snapshot.hasData){
-                 return 
-             crear(snapshot.data, context);
-                
-
-             
-              }
-              
-              else{
-              return CircularProgressIndicator();
-              }
-         
-            }
-
-        )
-        
-        
-        
-        ),
-        
-        
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        floatingActionButton: FloatingActionButton(
-          child: Container(
-           
-          child: Icon(Icons.camera_alt,
-          color: Theme.of(context).accentColor,
-          
+      appBar: AppBar(
+        title: Text('NUMERATE'),
+        centerTitle: true,
+        backgroundColor: Theme.of(context).accentColor,
+      ),
+      body: Center(
+          child: FutureBuilder(
+              future: obtener(),
+              builder: (context, AsyncSnapshot<List<Recursos>> snapshot) {
+                if (snapshot.hasData) {
+                  return crear(snapshot.data, context);
+                } else {
+                  return CircularProgressIndicator();
+                }
+              })),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        child: Container(
+          child: Icon(
+            Icons.camera_alt,
+            color: Theme.of(context).accentColor,
           ),
         ),
         onPressed: () =>
@@ -72,28 +51,31 @@ class Historial extends StatelessWidget {
     );
   }
 
-  Widget crear(List<Recursos> lista,BuildContext ctx) {
+  Widget crear(List<Recursos> lista, BuildContext ctx) {
     return ListView.builder(
-      
       itemCount: lista.length,
       itemBuilder: (BuildContext context, int index) {
-       
-
-        return 
-        
-        Card(
-                  child: ListTile(
-            leading: Image.network(
-              lista[index].imagenUrl,
-              width: 100.0,
-              height: 50.0,
+        return Card(
+          shape: BeveledRectangleBorder(
+            side: BorderSide(
+              color: Colors.grey,
+              width: 0.8,
             ),
+          ),
+          child: ListTile(
+            leading: Image.network(lista[index].imagenUrl,
+                width: 70.0, height: 70.0, fit: BoxFit.fitWidth),
             title: Text('Varillas conteo ' +
                 (index + 1).toString() +
                 '           ' +
-                '${lista[index].conteo == -1?'Cargando..':lista[index].conteo.toString()}'),
-            onTap: () => Navigator.of(ctx)
-                .pushNamed(MyApp.routedName, arguments: lista[index].id),
+                '${lista[index].conteo == -1 ? 'Cargando..' : lista[index].conteo.toString()}'),
+            onTap: () {
+              if (lista[index].conteo == -1 ){
+
+              }else{
+             Navigator.of(ctx)
+                .pushNamed(MyApp.routedName, arguments: lista[index].id);}
+            }
           ),
         );
       },
