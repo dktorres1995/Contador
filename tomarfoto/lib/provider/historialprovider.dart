e2bd1d11d304e518e6daa9273ecf4e298849aa04
dktorres1995 <59892053+dktorres1995@users.noreply.github.com>
2020-03-13 14:50:31 -0500

    Se coloca la imagen con puntos y se traen las coordenadas

--- tomarfoto/lib/provider/historialprovider.dart
+++ tomarfoto/lib/provider/historialprovider.dart
@@ -1,20 +1,38 @@
 
 import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
 import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
 import 'package:tomarfoto/Models/Recursos.dart';
-Future<Recursos> fetchPost(String id) async {
+Future<List<dynamic>> fetchPost(String id) async {
   final response = await http
       .get('https://object-counter.azurewebsites.net/contar/$id');
-
+  
+  final List<dynamic> lista =[];
   if (response.statusCode == 200) {
     // Si la llamada al servidor fue exitosa, analiza el JSON
-    return Recursos.fromJson(json.decode(response.body));
+    lista.add(Recursos.fromJson(json.decode(response.body)));
+    final resp =   await http
+      .get(Recursos.fromJson(json.decode(response.body)).imagenUrl); 
+    lista.add(resp);
+    return lista;
   } else {
     // Si la llamada no fue exitosa, lanza un error.
     throw Exception('Failed to load post');
   }
 }
+
+String timestamp() => DateTime.now().millisecondsSinceEpoch.toString();
+Future<String> ruta() async {
+  final Directory extDir = await getApplicationDocumentsDirectory();
+  final String dirPath = '${extDir.path}/Pictures/Dibujo_test';
+  await Directory(dirPath).create(recursive: true);
+
+  final String filePath = '$dirPath/${timestamp()}.jpg'; //
+  return filePath;
+}
+
 
 
 Future <List<Recursos>> obtener() async {
@@ -55,4 +73,12 @@ Future <List<Recursos>> obtener() async {
 }
 
 
+Future<http.Response> traerImagen(String url) async {
+   final response = await http.get(url);
+  if (response.statusCode == 200) {
+    return response;
+  } else {
+    throw ('errorCode: 200');
+  }
+}
 
