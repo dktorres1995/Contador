78855bdd3174e6be239e25c7af05ea7dee916f5c
Carlos Garzon <54120609+CarlosUngi@users.noreply.github.com>
2020-03-16 12:19:36 -0500

    zoom 1 funcionando

--- tomarfoto/lib/provider/providerConfig.dart
+++ tomarfoto/lib/provider/providerConfig.dart
@@ -1,5 +1,5 @@
  class ConfigPaths{
 
-//static const pathServicios = 'https://object-counter.azurewebsites.net';
- static const pathServicios = 'http://localhost:3000';
+static const pathServicios = 'https://object-counter.azurewebsites.net';
+// static const pathServicios = 'http://localhost:3000';
 }
