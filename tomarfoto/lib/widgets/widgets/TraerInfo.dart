347549b5955271a46d716a5e96d308477f92357a
Carlos Garzon <54120609+CarlosUngi@users.noreply.github.com>
2020-03-20 09:24:31 -0500

    zoom mejorado

--- tomarfoto/lib/widgets/widgets/TraerInfo.dart
+++ tomarfoto/lib/widgets/widgets/TraerInfo.dart
@@ -19,10 +19,7 @@ class _MyAppState extends State<MyApp> {
   List<Map<String, int>> prueba = List<Map<String, int>>();
   List<Map<String, int>> etEliminadas = List<Map<String, int>>();
   List<Map<String, int>> aEliminar = List<Map<String, int>>();
-  double _zoom = 0;
-  double _scale = 0;
-  double _offsetX = 0;
-  double _offsetY = 0;
+
   bool _editar = false;
   bool _eliminar = false;
   LibIma.Image imageMostrar;
@@ -39,26 +36,11 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
-  void cambiaScalayZoom(double scale, double zoom) {
-    _zoom = zoom;
-    _scale = scale;
-  }
 
-  void cambiaOffset(double x, double y) {
-    _offsetX = x;
-    _offsetY = y;
-  }
 
   void modificar(int dx, int dy, bool _eliminar) {
-    var xIn =
-        ((_offsetX < 0 ? _offsetX * 1.5 : _offsetX) + dx.toDouble() / _scale)
-            .round();
-    var yIn =
-        ((_offsetY < 0 ? _offsetY * 1.7 : _offsetY) + dy.toDouble() / _scale)
-            .round();
-    print(
-        'punto: x$xIn, y$yIn  y tamaño total: yT${imageMostrar.height}, xT${imageMostrar.width}');
-    print('escala $_scale zoom:$_zoom');
+    var xIn = dx;
+    var yIn = dy; 
 
     if (_eliminar) {
       setState(() {
@@ -120,50 +102,38 @@ class _MyAppState extends State<MyApp> {
           width: medida.maxWidth,
           child: Stack(
             children: <Widget>[
-              GestureDetector(
-                child: Container(
-                  height: medida.maxHeight,
-                  width: medida.maxWidth,
-                  child: Zoom(
-                      backgroundColor: Colors.white,
-                      initZoom: 0.0,
+              Zoom(
+                  backgroundColor: Colors.white,
+                  initZoom: 0.0,
+                  width: imageMostrar.width.toDouble(),
+                  height: imageMostrar.height.toDouble(),
+                  child: GestureDetector(
+                    child: Container(
+                      height:imageMostrar.height.toDouble(),
                       width: imageMostrar.width.toDouble(),
-                      height: imageMostrar.height.toDouble(),
-                      onPositionUpdate: (Offset position) {
-                        print('${position.dx} ' + '${position.dy}');
-
-                        cambiaOffset(position.dx, position.dy);
-                      },
-                      onScaleUpdate: (double scale, double zoom) {
-                        print("escala: $scale y zoom:  $zoom");
-
-                        cambiaScalayZoom(scale, zoom);
-                      },
-                      child: Center(
-                        child: Image.memory(LibIma.encodeJpg(imageMostrar)),
-                      )),
-                ),
-                onTapDown: (dato) {
-                  if (_editar) {
-                    modificar(dato.localPosition.dx.toInt(),
-                        dato.localPosition.dy.toInt(), false);
-                  }
-                  if (_eliminar) {
-                    modificar(dato.localPosition.dx.toInt(),
-                        dato.localPosition.dy.toInt(), true);
-                  }
-                },
-                onTapUp: (dato) {
-                  if (_editar) {
-                    modificar(dato.localPosition.dx.toInt(),
-                        dato.localPosition.dy.toInt(), false);
-                  }
-                  if (_eliminar) {
-                    modificar(dato.localPosition.dx.toInt(),
-                        dato.localPosition.dy.toInt(), true);
-                  }
-                },
-              ),
+                      child: Image.memory(LibIma.encodeJpg(imageMostrar)),
+                    ),
+                    onTapDown: (dato) {
+                      if (_editar) {
+                        modificar(dato.localPosition.dx.toInt(),
+                            dato.localPosition.dy.toInt(), false);
+                      }
+                      if (_eliminar) {
+                        modificar(dato.localPosition.dx.toInt(),
+                            dato.localPosition.dy.toInt(), true);
+                      }
+                    },
+                    onTapUp: (dato) {
+                      if (_editar) {
+                        modificar(dato.localPosition.dx.toInt(),
+                            dato.localPosition.dy.toInt(), false);
+                      }
+                      if (_eliminar) {
+                        modificar(dato.localPosition.dx.toInt(),
+                            dato.localPosition.dy.toInt(), true);
+                      }
+                    },
+                  )),
               circulo(
                   medida,
                   0.15,
